5c22d71da95ab0ab5f77e30260ea76503f0a676b
Marcos Zlotnik <marcoszlotnik@gmail.com>
2016-08-17 23:36:42 -0300

    Add files via upload

---
+++ bi.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+print "\n\nBI\n\n"
+now = datetime.now()
+print "YOU CAN WRITE YOUR REPORT\n\n"
+print (now)
+report = raw_input("ENTER:\n")
+save = open(str(now), "w")
+save.write(report)
+save.close()
+print "\n\nSAVED!\n\n"
